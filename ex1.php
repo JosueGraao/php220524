900568ce0f431eb5d8a9da5962e35cd78e515652
JosueGraao <miguelarno@protonmail.com>
2024-05-20 20:15:51 -0300

    Começando e empacando

--- ex1.php
+++ ex1.php
@@ -1,3 +1,14 @@
 <?php 
 
+$alunos = array("Miguel","Pedro","Alexandre","Silvio","Luciene","Lucia","Eunice","Luciano","José","Maria")
+$notas = array(8,6,9,5,10,9,8,7,8,5)
+
+for ($x=0;$x<=10;$x++){
+	$soma = $notas;
+}
+
+$media = $soma / 10;
+
+echo $media;
+
  ?>
