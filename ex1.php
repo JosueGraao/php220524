59d67bfcd3b3d22f567921b828e53f4b0f3e49f4
JosueGraao <miguelarno@protonmail.com>
2024-05-21 14:24:52 -0300

    Comentários no código

--- ex1.php
+++ ex1.php
@@ -14,10 +14,12 @@ $media = $soma / 10;
 
 $MaxNota = max($notas);
 
+//Abaixo, uso o array search para armazenar a posição da nota mais alta na variável $mAluno
 $mAluno = array_search(max($notas), $notas);
 
 echo "</br>A média desta classe é $media</br>";
 
+//Aqui, uso a variável $mAluno para exibir o nome correto do array $alunos
 echo "</br>O aluno com nota mais alta desta classe foi $alunos[$mAluno]";
 
  ?>
