7762945d961d755c43e294a94fd28c37ed97e3d4
JosueGraao <miguelarno@protonmail.com>
2024-05-21 15:36:42 -0300

    Tirei o x do ex3

--- ex3.php
+++ ex3.php
@@ -4,14 +4,12 @@
 
 $digitos = array (1,2,3,4,5,6,7,8,9,10);
 $mult = 3;
-$x = 0;
 
 echo "O usuário solicitou uma tabuada do $mult. Segue abaixo:</br>";
 
 foreach ($digitos as $seq){
-	$tab = $digitos[$x] * $mult;
+	$tab = $digitos[$seq-1] * $mult;
 	echo "$tab</br>";
-	$x++;
 }
 
  ?>
