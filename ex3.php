f369264c29e1fd846e40f5f6d850598e261447c6
JosueGraao <miguelarno@protonmail.com>
2024-05-21 14:59:53 -0300

    Ex3

--- ex3.php
+++ ex3.php
@@ -1,3 +1,15 @@
-<?php 
+<?php
+
+//Este é uma tabuada do 2
+
+$digitos = array (1,2,3,4,5,6,7,8,9,10);
+$mult = 3;
+
+echo "O usuário solicitou uma tabuada do $mult. Segue abaixo:</br>";
+
+for ($x = 0;$x <= 9;$x++){
+	$tab = $digitos[$x] * $mult;
+	echo "$tab</br>";
+}
 
  ?>
