63c2eaf87337924112fecc4c9aee6a367be0ab62
JosueGraao <145507671+JosueGraao@users.noreply.github.com>
2024-05-21 13:43:30 -0300

    Ajustes no Ex1
    Falta exibir o nome do aluno com nota mais alta.

--- ex1.php
+++ ex1.php
@@ -1,14 +1,21 @@
 <?php 
 
-$alunos = array("Miguel","Pedro","Alexandre","Silvio","Luciene","Lucia","Eunice","Luciano","José","Maria")
-$notas = array(8,6,9,5,10,9,8,7,8,5)
+$alunos = array("Miguel","Pedro","Alexandre","Silvio","Luciene","Lucia","Eunice","Luciano","José","Maria");
+$notas = array(8,6,9,5,10,9,8,7,8,5);
+$media = 0.0;
+$soma = 0.0;
 
-for ($x=0;$x<=10;$x++){
-	$soma = $notas;
+for ($x = 0;$x <= 9;$x++){
+	echo $alunos[$x].", Nota = ".$notas[$x]."</br>";
+	$soma += $notas[$x];
 }
 
 $media = $soma / 10;
 
-echo $media;
+$MaxNota = max($notas);
+
+echo "</br>A média desta classe é $media</br>";
+
+echo "</br>A nota mais alta desta classe foi $MaxNota"; 
 
  ?>
