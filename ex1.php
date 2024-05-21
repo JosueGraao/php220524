acd3b4f8e52d2b9b75014470cbffde88b6eb6b0d
JosueGraao <miguelarno@protonmail.com>
2024-05-21 15:24:10 -0300

    Troquei for por foreach e comentários

--- ex1.php
+++ ex1.php
@@ -1,13 +1,15 @@
 <?php 
 
-$alunos = array("Miguel","Pedro","Alexandre","Silvio","Luciene","Lucia","Eunice","Luciano","José","Maria");
-$notas = array(8,6,9,5,10,9,8,7,8,5);
+$alunos = array("Miguel","Pedro","Alexandre","Silvio","José","Lucia","Eunice","Luciano","Luciene","Maria");
+$notas = array(8,6,9,5,8,9,8,7,10,5);
+//A soma das notas dá 75. Logo, a média deve ser 7.5.
 $media = 0.0;
 $soma = 0.0;
+$x = 0;
 
-for ($x = 0;$x <= 9;$x++){
-	echo $alunos[$x].", Nota = ".$notas[$x]."</br>";
+foreach ($alunos as $seq){
 	$soma += $notas[$x];
+	$x++;
 }
 
 $media = $soma / 10;
@@ -17,9 +19,9 @@ $MaxNota = max($notas);
 //Abaixo, uso o array search para armazenar a posição da nota mais alta na variável $mAluno
 $mAluno = array_search(max($notas), $notas);
 
-echo "</br>A média desta classe é $media</br>";
+echo "A média desta classe é $media</br>";
 
 //Aqui, uso a variável $mAluno para exibir o nome correto do array $alunos
-echo "</br>O aluno com nota mais alta desta classe foi $alunos[$mAluno]";
+echo "</br>O aluno com nota mais alta desta classe foi $alunos[$mAluno] com nota $MaxNota.";
 
  ?>
